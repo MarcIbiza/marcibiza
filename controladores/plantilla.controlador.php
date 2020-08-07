cea454617a3851cc30dbc085fbc59983b03574cf
Marc <marc@efectoled.com>
2020-08-07 16:34:27 +0200

    Proyecto CMS

--- controladores/plantilla.controlador.php
+++ controladores/plantilla.controlador.php
@@ -1,14 +1,9 @@
 <?php
 
-Class ControladorPlantilla{
-
-	/*=============================================
-	Llamada a la plantilla
-	=============================================*/
+class ControladorPlantilla{
 
 	public function ctrTraerPlantilla(){
 
-		#include() Se utiliza para invocar el archivo que contiene código html-php.
 		include "vistas/plantilla.php";
 
 	}
