e89c6ecc792a00009f8f6a89621feb3eda87dd25
Marc <marc@efectoled.com>
2020-09-29 19:16:07 +0200

    Cambios

---
+++ modelos/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+class Conexion{
+
+	static public function conectar(){
+
+		$link = new PDO("mysql:host=localhost:3306;dbname=blog-php",
+						"marcibiz",
+						"r742iUOlPR8[g;");
+
+		$link->exec("set names utf8");
+
+		return $link;
+
+	}
+
+
+}
